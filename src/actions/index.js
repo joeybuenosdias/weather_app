8455a5f52ddf3eec1f82c12cffa8e9ede2a96893
Joey Schrader <joedanproperty@gmail.com>
2018-01-17 21:37:47 -0700

    googlemap api working, project finished

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_KEY = 'a648c5da9efff655967afa6ad92750fc';
+//MAKE SURE TO IMPORT OPEN WEATHER APP API HERE!!!!!
 const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
 
 export const FETCH_WEATHER = 'FETCH_WEATHER';
